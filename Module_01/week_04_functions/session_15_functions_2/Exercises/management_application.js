d5b2066ea53233b5e88b272a1d89fb4ac06479f5
anh.tran(c0720i1) <69283798+theanh111@users.noreply.github.com>
2020-08-25 10:40:32 +0700

    update

--- Module_01/week_04_functions/session_15_functions_2/Exercises/management_application.js
+++ Module_01/week_04_functions/session_15_functions_2/Exercises/management_application.js
@@ -1,38 +1,64 @@
-let User = [];
-let order = 0;
-drawTable(User);
+let display = document.getElementById("table");
+let drinksName = ["Cafe Sữa Đá",
+    "Cafe Kem Cheese",
+    "Cafe Đen",
+    "Bạc Sỉu",
+    "Cafe Kem Đá Xay",
+    "Capucchino",
+    "Matcha Latte",
+    "Chocolate Latte",
+    "Cacao"];
+drawTable(drinksName);
 
 function drawTable(array) {
     let table;
-    let row = array.length;
-    let numbers = row;
     table = "<table>";
-    table = table + "<tr>";
-    table = table + "<th>" + "Order" + "</th>";
-    table = table + "<th>" + "Drink(s) name" + "</th>";
-    table = table + "<th>" + "</th>";
-    table = table + "<th>" + numbers + " Drinks" + "</th>";
-    table = table + "<th>" + "</th>";
-    table = table + "</tr>";
-    for (let i = 0; i < row; i++) {
+    table = table + "<tr>" +
+                             "<th width='10px'>No.1</th>" +
+                             "<th>Drink(s)</th>" +
+                             "<th></th>" +
+                             "<th style='text-align: right'>" + "Total: " + array.length + "&nbsp;" + "Drink(s)" + "</th>" +
+                    "</tr>";
+    for (let i = 0; i < array.length; i++) {
         table = table + "<tr>";
-        for (let j = 0; j < 4; j++) {
-            table = table + "<td>" + array[i][j] + "</td>";
-        }
+        table = table + "<th>" + [i + 1] + "</th>";
+        table = table + "<td>" + array[i] + "</td>";
+        table = table + "<td>" +  "<button onclick='editDrinks("+ i +");'>Edit" + "</button>" + "</td>";
+        table = table + "<td>" +  "<button onclick='deleteDrinks("+ i +");'>Delete" + "</button>" + "</td>";
         table = table + "</tr>";
     }
-    document.getElementById("table").innerHTML = table;
+    table = table + "</table>"
+    display.innerHTML = table;
 }
 
-function addProducts() {
+function addDrinks() {
+    let inputDrink = document.getElementById("adddrinks").value;
+    drinksName[drinksName.length] = inputDrink;
+    if (inputDrink === "" || inputDrink === " ") {
+        alert("Please input name of your drink!");
+    } else {
+        drawTable(drinksName);
+    }
 
 }
 
 
-function editProducts() {
-
+function editDrinks(currentIndex) {
+    let editName = prompt("Changing your drink's name here: ","");
+    drinksName[currentIndex] = editName;
+    if (editName === "" || editName === " ") {
+        alert("Please type something!");
+    }
+    drawTable(drinksName);
 }
 
-function deleteProducts() {
-
+function deleteDrinks(index) {
+    let deleteConfirm = confirm("Are you sure about that?!!");
+    if (deleteConfirm) {
+        drinksName.splice(index, 1);
+        alert("You have deleted your drink!");
+    } else {
+        alert("You haven't deleted your drink!");
+    }
+    drawTable(drinksName);
 }
